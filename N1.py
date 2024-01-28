35730dfdd57a9da53947b23dd4748df959c18d65
Ekimov Sergey <es281082@ukr.net>
2024-01-28 09:14:30 +0200

    HW3_1

---
+++ N1.py
@@ -0,0 +1,13 @@
+import datetime as dt
+from datetime import datetime as dtdt
+
+def get_days_from_today(date):
+    try:
+        date_p = dtdt.strptime(date,"%Y-%m-%d")
+    except ValueError:
+        return 'date must be "YYYY-MM-DD" format'
+        
+    date_now = dtdt.today()
+    return (date_now-date_p).days
+    
+print(get_days_from_today("20-12-19"))
